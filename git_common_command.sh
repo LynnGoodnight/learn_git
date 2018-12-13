b94bb2a2d7a9753f5edb19914937dfca9ce35b2a
yangliu <32932866LynnGoodnight@users.noreply.github.com>
2018-12-13 14:06:08 +0800

    write a git_common_command.sh file

---
+++ git_common_command.sh
@@ -0,0 +1,9 @@
+#在git bash窗口可以使用Linux命令,感觉就像在Linux系统下操作一样
+git config --global user.name "yangliu"
+git config --global user.email "32932866LynnGoodnight@users.noreply.github.com"
+cd D:\
+mkdir learn_git
+cd learn_git
+pwd
+git init
+git add git_common_command.sh
